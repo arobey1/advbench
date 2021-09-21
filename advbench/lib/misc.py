b4f8f1e624a8129b92d2e22b9d61bbca405191a3
Alex Robey <arobey1@seas.upenn.edu>
2021-09-21 13:52:03 -0400

    functions to compute accuracy

--- advbench/lib/misc.py
+++ advbench/lib/misc.py
@@ -1,3 +1,4 @@
+import torch
 import hashlib
 
 def seed_hash(*args):
@@ -5,3 +6,36 @@ def seed_hash(*args):
 
     args_str = str(args)
     return int(hashlib.md5(args_str.encode("utf-8")).hexdigest(), 16) % (2**31)
+
+@torch.no_grad()
+def accuracy(algorithm, loader, device):
+    correct, total = 0, 0
+
+    algorithm.eval()
+    for imgs, labels in loader:
+        imgs, labels = imgs.to(device), labels.to(device)
+        output = algorithm.predict(imgs)
+        pred = output.argmax(dim=1, keepdim=True)
+        correct += pred.eq(labels.view_as(pred)).sum().item()
+        total += imgs.size(0)
+    algorithm.train()
+
+    return 100. * correct / total
+
+def adv_accuracy(algorithm, loader, device, attack):
+    correct, total = 0, 0
+
+    algorithm.eval()
+    for imgs, labels in loader:
+        imgs, labels = imgs.to(device), labels.to(device)
+        adv_imgs = attack(imgs, labels)
+
+        with torch.no_grad():
+            output = algorithm.predict(adv_imgs)
+            pred = output.argmax(dim=1, keepdim=True)
+
+        correct += pred.eq(labels.view_as(pred)).sum().item()
+        total += imgs.size(0)
+    algorithm.train()
+
+    return 100. * correct / total
