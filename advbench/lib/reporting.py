d9263ec737c8dd70d22ffebaa7104c9d51594818
Alex Robey <arobey1@seas.upenn.edu>
2021-09-22 18:54:23 -0400

    fixed bug so that we don't look for results in results.txt

--- advbench/lib/reporting.py
+++ advbench/lib/reporting.py
@@ -6,7 +6,12 @@ def load_records(path):
     records = []
     for i, subdir in tqdm.tqdm(list(enumerate(os.listdir(path))), ncols=80, leave=False):
         results_path = os.path.join(path, subdir, 'results.pkl')
-        df = pd.read_pickle(results_path)
-        records.append(df)
+
+        # want to ignore existing results.txt file
+        try:
+            df = pd.read_pickle(results_path)
+            records.append(df)
+        except IOError:
+            pass
 
     return pd.concat(records, ignore_index=True)
