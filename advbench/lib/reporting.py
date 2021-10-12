48a3a08a1bb80740650d584336ab05e9050b0a8c
Alex Robey <arobey1@seas.upenn.edu>
2021-10-12 08:40:51 -0400

    added option to pass name of results file

--- advbench/lib/reporting.py
+++ advbench/lib/reporting.py
@@ -2,7 +2,9 @@ import os
 import tqdm
 import pandas as pd
 
-def load_records(path, depth=1):
+def load_records(path, results_fname='results', depth=1):
+    assert results_fname in ['results', 'meters']
+
     records = []
 
     def add_record(results_path):
@@ -15,12 +17,12 @@ def load_records(path, depth=1):
             pass
 
     if depth == 0:
-        results_path = os.path.join(path, 'results.pkl')
+        results_path = os.path.join(path, f'{results_fname}.pkl')
         add_record(results_path)
 
     elif depth == 1:
         for i, subdir in tqdm.tqdm(list(enumerate(os.listdir(path))), ncols=80, leave=False):
-            results_path = os.path.join(path, subdir, 'results.pkl')
+            results_path = os.path.join(path, subdir, f'{results_fname}.pkl')
             add_record(results_path)
 
     else:
