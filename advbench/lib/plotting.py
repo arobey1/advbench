159833af5bcc57aef0f10e4985a856f252446e8a
Alex Robey <arobey1@seas.upenn.edu>
2023-02-16 13:19:37 -0500

    functionality to show values at top of bar chart

--- advbench/lib/plotting.py
+++ advbench/lib/plotting.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 def remove_legend_title(ax, name_dict=None, fontsize=16):
     handles, labels = ax.get_legend_handles_labels()
@@ -22,3 +23,24 @@ def tick_density(plot, every=2, mod_val=1, axis='x'):
             label.set_visible(True)
         else:
             label.set_visible(False)
+
+def show_bar_values(axs, orient="v", space=.01):
+    def _single(ax):
+        if orient == "v":
+            for p in ax.patches:
+                _x = p.get_x() + p.get_width() / 2
+                _y = p.get_y() + p.get_height() + (p.get_height()*0.01)
+                value = '{:.3f}'.format(p.get_height())
+                ax.text(_x, _y, value, ha="center") 
+        elif orient == "h":
+            for p in ax.patches:
+                _x = p.get_x() + p.get_width() + float(space)
+                _y = p.get_y() + p.get_height() - (p.get_height()*0.5)
+                value = '{:.3f}'.format(p.get_width())
+                ax.text(_x, _y, value, ha="left")
+
+    if isinstance(axs, np.ndarray):
+        for idx, ax in np.ndenumerate(axs):
+            _single(ax)
+    else:
+        _single(axs)
