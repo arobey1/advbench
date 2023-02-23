d213953c2e94fa56278481d40bf6d47e957be5e0
Alex Robey <arobey1@seas.upenn.edu>
2023-02-23 11:41:54 -0500

    reporting only for selection dataframe

--- advbench/lib/reporting.py
+++ advbench/lib/reporting.py
@@ -1,32 +1,41 @@
 import os
 import tqdm
+import json
 import pandas as pd
+from typing import List
 
-def load_records(path, results_fname='results', depth=1):
-    assert results_fname in ['results', 'meters', 'losses']
+def load_record(path: str) -> List[dict]:
+    """Load the JSON stored in a given path to a list."""
+
+    records = []
+    with open(path, 'r') as f:
+        for line in f:
+            records.append(json.loads(line[:-1]))
+    return records
+
+def load_sweep_dataframes(path, depth=1):
 
     records = []
 
     def add_record(results_path):
         try:
-            df = pd.read_pickle(results_path)
-            records.append(df)
+            records.append(pd.read_pickle(results_path))
 
-        # want to ignore existing results.txt file
+        # want to ignore existing files 
+        # (e.g., results.txt, args.json, etc.)
         except IOError:
             pass
 
     if depth == 0:
-        results_path = os.path.join(path, f'{results_fname}.pkl')
+        results_path = os.path.join(path, f'selection.pd')
         add_record(results_path)
 
     elif depth == 1:
-        for i, subdir in tqdm.tqdm(list(enumerate(os.listdir(path))), ncols=80, leave=False):
-            results_path = os.path.join(path, subdir, f'{results_fname}.pkl')
+        for i, subdir in list(enumerate(os.listdir(path))):
+            results_path = os.path.join(path, subdir, f'selection.pd')
             add_record(results_path)
 
     else:
         raise ValueError(f'Depth {depth} is invalid.')
-
 
     return pd.concat(records, ignore_index=True)
